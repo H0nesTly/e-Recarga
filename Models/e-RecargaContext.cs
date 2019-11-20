3a7edc279959843b56eee6c17337a4fe2b585b54
H0nesTly <jfgaming69@gmail.com>
2019-11-20 23:43:22 +0000

    Criar contexto da base de dados da aplicaçao

---
+++ Models/e-RecargaContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace e_Recarga.Models
+{
+    public class e_RecargaContext :
+        DbContext
+    {
+        public e_RecargaContext():
+            base("e_RecargaDB")
+        {
+            //https://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
+            Database.SetInitializer<e_RecargaContext>(new CreateDatabaseIfNotExists<e_RecargaContext>());
+
+
+        }
+
+        public DbSet<Utilizador> Utilizadores { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<ContaBancaria> ContaBancarias { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Carregamento> Carregamentos { get; set; }
+        public DbSet<Posto> Postos { get; set; }
+        public DbSet<EstacaoCarregamento> EstacaoCarregamentos { get; set; }
+        public DbSet<Localizacao> Localizacaos { get; set; }
+    }
+}
