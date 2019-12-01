4256f800112990886f87d7979f95fb7d701f37ce
joao <jfgaming69@gmail.com>
2019-12-01 20:05:49 +0000

    Gerar CRUD para os veiculos

---
+++ Controllers/VeiculosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using e_Recarga.Models;
+
+namespace e_Recarga.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Veiculos
+        public ActionResult Index()
+        {
+            var veiculos = db.Veiculos.Include(v => v.UtilizadorVeiculo);
+            return View(veiculos.ToList());
+        }
+
+        // GET: Veiculos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Veiculo veiculo = db.Veiculos.Find(id);
+            if (veiculo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(veiculo);
+        }
+
+        // GET: Veiculos/Create
+        public ActionResult Create()
+        {
+            ViewBag.UtilizadorID = new SelectList(db.ApplicationUsers, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Veiculos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Matricula,Marca,Modelo,UtilizadorID")] Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Veiculos.Add(veiculo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UtilizadorID = new SelectList(db.ApplicationUsers, "Id", "Nome", veiculo.UtilizadorID);
+            return View(veiculo);
+        }
+
+        // GET: Veiculos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Veiculo veiculo = db.Veiculos.Find(id);
+            if (veiculo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UtilizadorID = new SelectList(db.ApplicationUsers, "Id", "Nome", veiculo.UtilizadorID);
+            return View(veiculo);
+        }
+
+        // POST: Veiculos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Matricula,Marca,Modelo,UtilizadorID")] Veiculo veiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(veiculo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UtilizadorID = new SelectList(db.ApplicationUsers, "Id", "Nome", veiculo.UtilizadorID);
+            return View(veiculo);
+        }
+
+        // GET: Veiculos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Veiculo veiculo = db.Veiculos.Find(id);
+            if (veiculo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(veiculo);
+        }
+
+        // POST: Veiculos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Veiculo veiculo = db.Veiculos.Find(id);
+            db.Veiculos.Remove(veiculo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
